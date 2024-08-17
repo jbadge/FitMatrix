31b31db005d137cdc9b58d99a3a4e822c9487889
Jeremy Badger <jeremebadger@gmail.com>
2024-08-17 12:35:12 -0700

    Static html build out for Calories page

--- ClientApp/src/main.tsx
+++ ClientApp/src/main.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
 import './index.scss'
 import { App } from './App'
 
-ReactDOM.render(
+const queryClient = new QueryClient()
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Router>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </Router>
+  </React.StrictMode>
 )
